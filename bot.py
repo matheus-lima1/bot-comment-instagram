b5d660e6de7c2b2f32684d255b53591b604352de
Matheus Lima <matheus.lima@unifei.edu.br>
2020-04-02 19:34:34 -0300

    using-terminal
    Usando o terminal para receber url e número de comentários utilizados para cada publicação

--- bot.py
+++ bot.py
@@ -4,34 +4,37 @@ import time
 import random
 
 class Instagram:
-    def __init__(self, nomeUsuario, senha):
-        self.nomeUsuario = nomeUsuario ## username
-        self.senha = senha ## passwod
-        self.driver = webdriver.Firefox(executable_path="local do geckodriver.exe")
+    def __init__(self, nomeUsuario, senha,publicacao,num):
+        self.num = num
+        self.nomeUsuario = nomeUsuario 
+        self.senha = senha 
+        # escreva o caminho do arquivo geckodriver.exe usando duas barras "\\"
+        self.driver = webdriver.Firefox(executable_path="caminho\\geckodriver.exe") 
+        self.publicacao = publicacao
 
     def login(self):
         time.sleep(5)
         driver = self.driver
-        driver.get("https://www.instagram.com") ##chama o site requerido
+        driver.get("https://www.instagram.com") 
 
         time.sleep(5)
 
-        form_usuario = driver.find_element_by_xpath('//input[@name="username"]') ## PROCURAR CAMPO DO FORMULÁRIO DE USER
-        form_usuario.click() ##CLICAR NO CAMPO SELECIONADO
+        form_usuario = driver.find_element_by_xpath('//input[@name="username"]') 
+        form_usuario.click() 
         time.sleep(5)
-        form_usuario.clear() ##Limpar campo
-        form_usuario.send_keys(self.nomeUsuario) ## preencher campo
+        form_usuario.clear() 
+        form_usuario.send_keys(self.nomeUsuario)
         time.sleep(5)
 
-        form_senha = driver.find_element_by_xpath("//input[@name='password']") ## PROCURAR CAMPO DO FORMULÁRIO DE SENHA
+        form_senha = driver.find_element_by_xpath("//input[@name='password']") 
         form_senha.click()
         time.sleep(5)
         form_senha.clear()
         form_senha.send_keys(self.senha)
         time.sleep(5)
-        form_senha.send_keys(Keys.RETURN) ## SIMULA O ENTER
+        form_senha.send_keys(Keys.RETURN) 
         time.sleep(5)
-        self.comentar('calistenia')
+        self.comentar()
         time.sleep(5)
 
 
@@ -41,47 +44,56 @@ class Instagram:
             local.send_keys(letra)
             time.sleep(random.randint(2,8)/30)
 
-    def comentar(self,hashtag):
+    def comentar(self):
 
-        ##procura a # que queremos pesquisar
+ 
         driver = self.driver
         time.sleep(5)
-        driver.get("https://www.instagram.com/explore/tags/"+ hashtag +"/")
+        driver.get(self.publicacao)
 
-        for i in range(1,3):
+
+
+        for i in range(1,2):
             driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
             time.sleep(5)
 
-        links = driver.find_elements_by_tag_name('a') ## procura todos os elementos a
-        time.sleep(5)
-        link_fotos = [elem.get_attribute('href') for elem in links] ## recupera elementos uteis    
-        [href for href in link_fotos if hashtag in href] ## nova filtragem
-        time.sleep(5)
 
-        print(hashtag + ' fotos ' + str(len(link_fotos)))
+        try:
+            #insira os comentários desejados nesta lista
+            comentarios = ["It's great! 1 ","I love 2 ","lindo ! 3 ", "start 4 ","the best 5 ", "yeees 6"] 
+            c=0
 
-        for link_foto in link_fotos:
-            driver.get(link_foto)
-            driver.execute_script('window.scrollTo(0, document.body.scrollHeight);') ## abaixar tela
-            try:
-                comentarios = ["It's great!","Very good!!"]
-                driver.find_element_by_class_name('Ypffh').click()
-                form_comentario = driver.find_element_by_class_name('Ypffh')
-                time.sleep(random.randint(3,8))
-                self.digitar(random.choice(comentarios),form_comentario)
-                time.sleep(random.randint(30,40))
-                driver.find_element_by_xpath("//button[contains(text(),'Publicar')]").click()
+            for i in range(int(len(comentarios))):
                 time.sleep(5)
-            except Exception as e:
-                print(e)
-                time.sleep(5)   
+                c = c+1
+                if(c!=int(self.num)):
+ 
+                    driver.find_element_by_xpath("//textarea[@aria-label='Adicione um comentário...']").click()
+                    form_comentario = driver.find_element_by_xpath("//textarea[@aria-label='Adicione um comentário...']")
+                    time.sleep(random.randint(3,8))
+                    self.digitar(comentarios[i],form_comentario)
+                    time.sleep(random.randint(5,8))
+                    
+                else:
+                    
+                    driver.find_element_by_xpath("//textarea[@aria-label='Adicione um comentário...']").click()
+                    form_comentario = driver.find_element_by_xpath("//textarea[@aria-label='Adicione um comentário...']")
+                    time.sleep(random.randint(3,8))
+                    self.digitar(comentarios[i],form_comentario)
+                    time.sleep(random.randint(5,8))
+
+                    driver.find_element_by_xpath("//button[contains(text(),'Publicar')]").click()
+                    time.sleep(5)
+                    c=0
+        except Exception as e:
+            print(e)
+            time.sleep(5) 
+            
 
 
+url = input("informe a URL: ")
+n = int(input("informe o número de comentários: "))
 
-
-        
-
-
-
-bot1 = Instagram('seu_user','sua_senha')
+# complete com seus dados
+bot1 = Instagram('seu_user','sua_senha',url,n)
 bot1.login()
